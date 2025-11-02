d1ef8bd93d7fd7574ad4ad8888d87909cc373cac
Barbora Smetanová <smetanovabarbora@email.cz>
2025-11-02 22:49:27 +0100

    css styly

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import "./Registration.css";
 
 export interface User {
   username: string;
-  email: string | number;
-  password: number | string;
-  passwordConfirm: number | string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
 }
 
 export const Registration = () => {
@@ -42,44 +43,45 @@ export const Registration = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="Email">Email</label>
-      <input
-        type="text"
-        name="email"
-        placeholder="Email"
-        id="email"
-        value={user.email}
-        onChange={handleChange}
-      />
-      <label htmlFor="username">Jméno uživatele:</label>
-      <input
-        type="text"
-        name="username"
-        placeholder="jméno uživatele"
-        id="username"
-        value={user.username}
-        onChange={handleChange}
-      />
-      <label htmlFor="password">Heslo</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        placeholder="zadej heslo"
-        value={user.password}
-        onChange={handleChange}
-      />
-      <label htmlFor="confirm">Potvrzení hesla:</label>
-      <input
-        type="password"
-        id="passwordConfirm"
-        name="passwordConfirm"
-        placeholder="zadej heslo znovu."
-        value={user.passwordConfirm}
-        onChange={handleChange}
-      />
-
-      <button type="submit">Register</button>
+      <div>
+        <label htmlFor="Email">Email</label>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          id="email"
+          value={user.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="username">Jméno uživatele:</label>
+        <input
+          type="text"
+          name="username"
+          placeholder="jméno uživatele"
+          id="username"
+          value={user.username}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Heslo</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="zadej heslo"
+          value={user.password}
+          onChange={handleChange}
+        />
+        <label htmlFor="confirm">Potvrzení hesla:</label>
+        <input
+          type="password"
+          id="passwordConfirm"
+          name="passwordConfirm"
+          placeholder="zadej heslo znovu."
+          value={user.passwordConfirm}
+          onChange={handleChange}
+        />
+        <button type="submit">Register</button>
+      </div>
     </form>
   );
 };
